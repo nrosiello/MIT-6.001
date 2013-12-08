5117bee09354fe6ded307bfc8d7ac0bcae7a381a
Nick Rosiello <nickrosiello@gmail.com>
2013-12-08 11:08:11 -0600

    Project 4, exercises 1 and 2: Exploration of hierarchy of the avatar object.  Addition of the HAS-A and HAS-A-THING-NAMED methods to test which objects that people contain.

--- project-4/project-4.scm
+++ project-4/project-4.scm
@@ -54,3 +54,58 @@
 ;; a room is randomly picked for each person to start out in.  the things are
 ;; placed in the the same room each time.  starting rooms are different between
 ;; the things.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; to get to the thing part, you ahve to go up the inheritance hierarchy from
+;; the person part, to the mobile-thing part, to the thing part.
+;; output from showing the thing part:
+; HANDLER: #[compound-procedure 34 handler]
+; TYPE: thing
+;(methods (install #[compound-procedure 38])
+;                  (location #[compound-procedure 37])
+;                  (destroy #[compound-procedure 36])
+;                  (emit #[compound-procedure 35]))
+; Parent frame: #[environment 39]
+; named-part:   #[compound-procedure 40 handler]
+;    Parent frame: global-environment 
+;    self:         (instance #[compound-procedure 13 handler])
+;    name:         nick
+;    location:     (instance #[compound-procedure 19 handler])
+
+;; after moving from the birthplace of the avatar, the location of the
+;; mobile-thing changes to the new location.
+
+;; the value of the self variable for the superclass handlers of the avatar class
+;; does not ever change, it always points to the base avatar class instance.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; testing of two new methods for people:
+;; HAS-A method to determine if a person has any things of a given type
+;; HAS-A-THING-NAMED method to determine if person has things with a given name
+(begin
+  (setup 'nick)
+  (define loc (ask me 'location))
+  (define bob (create-person 'bob loc))
+  (define spell-1 (create-spell 'spell-1 loc '() '()))
+  (define spell-2 (create-spell 'spell-2 loc '() '()))
+
+  ;; test that bob initially has no spells
+  (test-equal (ask bob 'HAS-A 'spell) '())
+
+  (ask bob 'ADD-THING spell-1)
+  (ask bob 'ADD-THING spell-2)
+  
+  ;; test that now bob has spells
+  (test-equal (ask bob 'HAS-A 'spell) (list spell-2 spell-1))
+  (test-equal (ask bob 'HAS-A 'fake-type) (list))
+
+  ;; test has-a-thing-named method
+  (test-equal (ask bob 'HAS-A-THING-NAMED 'fake-name) (list))
+  (test-equal (ask bob 'HAS-A-THING-NAMED 'spell-1) (list spell-1))
+)
